57a45423f984de0b3c7765da1a5ff463ff202bdb
Ol_Kuts <kuts.ol.vas@gmail.com>
2021-09-13 05:33:21 +0300

    fix: verification

--- src/pages/globalModules/ferifyToken.js
+++ src/pages/globalModules/ferifyToken.js
@@ -1,16 +1,25 @@
 import { api } from './api';
 
+const isLoggedOut = !location.pathname.includes('/index') && !location.pathname.includes('/sign-up') && location.pathname !== '/';
+const isLoggedIn = location.pathname.includes('/index') || location.pathname.includes('/sign-up') || location.pathname === '/';
+
 export const verifyToken = async () => {
   const token = localStorage.getItem('token');
   if (token) {
     try {
       await api.verifyToken(token);
-      if (location.pathname === '/' || location.pathname === '/sign-up.html') location.href = '/dashboard.html';
+      if (isLoggedIn) {
+        location.href = 'dashboard.html';
+      }
     } catch (error) {
+      if (!isLoggedOut) {
+        location.href = 'index.html';
+      }
       localStorage.removeItem('token');
-      location.href = '/';
     }
   } else {
-    if (location.pathname !== '/' && location.pathname !== '/sign-up.html') location.href = '/';
+    if (isLoggedOut) {
+      location.href = 'index.html';
+    }
   }
 };
--- src/pages/leads/scripts/api.js
+++ src/pages/leads/scripts/api.js
@@ -8,47 +8,10 @@ export const api = Object.freeze({
         'Content-Type': 'application/json',
         'x-token': token,
       },
-      body: pipeline,
+      body: JSON.stringify({ pipeline }),
     });
     const { data } = await response.json();
 
     return data;
   },
-
-  // loadPriorities: async (token) => {
-  //   const response = await fetch(`${base}/settings/tags`, {
-  //     headers: {'x-token': token},
-  //   });
-  //   const { data } = await response.json();
-  //
-  //   return data;
-  // },
-
-  // loadLeads: async (token, limit, page) => {
-  //   const response = await fetch(`${base}/leads?limit=${limit}&page=${page}`, {
-  //     headers: {'x-token': token},
-  //   });
-  //   const { data } = await response.json();
-  //
-  //   return data;
-  // },
-  //
-  // loadUsers: async (token, limit, page) => {
-  //   const response = await fetch(`${base}/users?limit=${limit}&page=${page}`, {
-  //     headers: {'x-token': token},
-  //   });
-  //   const { data } = await response.json();
-  //
-  //   return data;
-  // },
-  //
-  // loadLeads: async (token, limit, page) => {
-  //   const response = await fetch(`${base}/leads?limit=${limit}&page=${page}`, {
-  //     headers: {'x-token': token },
-  //   });
-  //   const { data } = await response.json();
-  //
-  //   return data;
-  // },
-
 });
